bbe0756ab944357fb496766181f94e869ad79e1a
Warboss-rus <Urgaksh@rambler.ru>
2015-03-27 22:26:42 +0300

    Added parsing of model types

--- src/com/example/wh40k/W40kModelType.java
+++ src/com/example/wh40k/W40kModelType.java
@@ -38,9 +38,62 @@ public class W40kModelType implements Parcelable {
         if(lowerStr.contains("vehicle")) {
             type=BasicType.VEHICLE;
         }
+        if(lowerStr.contains("jump infantry")) {
+            type=BasicType.JUMP_INFANTRY;
+        }
+        if(lowerStr.contains("jetpack")) {
+            type=BasicType.JETPACK_INFANTRY;
+        }
+        if(lowerStr.contains("bike")) {
+            type=BasicType.BIKE;
+        }
+        if(lowerStr.contains("jetbike")) {
+            type=BasicType.JETBIKE;
+        }
+        if(lowerStr.contains("beast")) {
+            type=BasicType.BEAST;
+        }
+        if(lowerStr.contains("calvary")) {
+            type=BasicType.CALVARY;
+        }
+        if(lowerStr.contains("artillery")) {
+            type=BasicType.ARTILLERY;
+        }
+        if(lowerStr.contains("flying monstrous creature")) {
+            type=BasicType.FLYING_MONSTROUS_CREATURE;
+        } else if(lowerStr.contains("monstrous creature")) {
+            type=BasicType.MONSTROUS_CREATURE;
+        }
+        if(lowerStr.contains("super-heavy")) {
+            type=BasicType.SUPER_HEAVY_VEHICLE;
+        }
         modifiers = new ArrayList<TypeModifiers>();
         if(lowerStr.contains("walker")) {
             modifiers.add(TypeModifiers.WALKER);
+        }
+        if(lowerStr.contains("character")) {
+            modifiers.add(TypeModifiers.CHARACTER);
+        }
+        if(lowerStr.contains("fast")) {
+            modifiers.add(TypeModifiers.FAST);
+        }
+        if(lowerStr.contains("skimmer")) {
+            modifiers.add(TypeModifiers.SKIMMER);
+        }
+        if(lowerStr.contains("tank")) {
+            modifiers.add(TypeModifiers.TANK);
+        }
+        if(lowerStr.contains("open-topped")) {
+            modifiers.add(TypeModifiers.OPEN_TOPPED);
+        }
+        if(lowerStr.contains("transport")) {
+            modifiers.add(TypeModifiers.TRANSPORT);
+        }
+        if(lowerStr.contains("flyer")) {
+            modifiers.add(TypeModifiers.FLYER);
+        }
+        if(lowerStr.contains("hover")) {
+            modifiers.add(TypeModifiers.HOVER);
         }
     }
 
@@ -71,6 +124,16 @@ public class W40kModelType implements Parcelable {
         WALKER,
         FLYER,
         HOVER,
+    }
+
+    public String toString() {
+        String result = type.toString() + " (";
+        for(TypeModifiers modifier : modifiers) {
+            result += modifier.toString() + ", ";
+        }
+        result = result.substring(0, result.length() - 2);
+        if(modifiers.size() > 0) result += ")";
+        return result;
     }
 
     public static final Parcelable.Creator<W40kModelType> CREATOR = new Parcelable.Creator<W40kModelType>(){
